42e47c6b2d11ddf58d02738815e751412c90e584
Crowdbotics <team@crowdbotics.com>
2020-06-12 07:45:00 +0000

    Update models

--- users/migrations/0003_ggjyu.py
+++ users/migrations/0003_ggjyu.py
@@ -8,15 +8,30 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0002_auto_20200612_0704'),
+        ("users", "0002_auto_20200612_0704"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Ggjyu',
+            name="Ggjyu",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('sdgadfhs', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='ggjyu_sdgadfhs', to=settings.AUTH_USER_MODEL)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "sdgadfhs",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="ggjyu_sdgadfhs",
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
             ],
         ),
     ]
